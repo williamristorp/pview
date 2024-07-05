7ad9f17a9d9f3fde67fe8a16b29dff77d101b2ac
William Ristorp Heidelberg <william@heidelberg.nu>
2024-07-05 08:48:11 +0000

    Add size flag and display percentage done

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use clap::Parser;
-use pview::human_bytes::{format_bytes, format_transfer_rate};
+use pview::human_bytes::{format_bytes, format_transfer_rate, parse_bytes};
 
 const BUFFER_SIZE: usize = 65536;
 
@@ -14,16 +14,25 @@ struct Cli {
         short,
         long,
         value_name = "SECS",
-        help = "Wait SECS seconds between updates (decimals are allowed).",
+        help = "Wait SECS seconds between updates",
         default_value = "1.0"
     )]
     interval: f64,
+
+    #[arg(
+        short,
+        long,
+        value_name = "SIZE",
+        help = "Assume the input data will be SIZE bytes long (you may append binary (base 1024) units such as `K`, `M` or `G`)"
+    )]
+    size: Option<String>,
 }
 
 fn main() {
     let cli = Cli::parse();
 
     let start_time = Instant::now();
+    let total_size = cli.size.and_then(|s| parse_bytes(&s));
     let mut last_progress_time = start_time;
     let mut buffer = [0; BUFFER_SIZE];
     let mut total_bytes_read: u128 = 0;
@@ -46,11 +55,21 @@ fn main() {
             let transfer_rate =
                 format_transfer_rate(total_bytes_read / start_time.elapsed().as_secs() as u128);
 
-            eprintln!(
-                "TOTAL: {}, RATE: {}",
-                format_bytes(total_bytes_read),
-                transfer_rate,
-            );
+            if let Some(size) = total_size {
+                eprintln!(
+                    "TOTAL: {:>9} / {} ({:.2}%), RATE: {}",
+                    format_bytes(total_bytes_read),
+                    format_bytes(size),
+                    total_bytes_read as f64 / size as f64 * 100.0,
+                    transfer_rate,
+                );
+            } else {
+                eprintln!(
+                    "TOTAL: {:>9}, RATE: {}",
+                    format_bytes(total_bytes_read),
+                    transfer_rate,
+                );
+            }
 
             last_progress_time = Instant::now();
         }
@@ -69,7 +88,7 @@ fn main() {
         format_transfer_rate(total_bytes_read / start_time.elapsed().as_secs() as u128);
 
     eprintln!(
-        "DONE! TOTAL: {}, ELAPSED: {} s, RATE: {}",
+        "DONE! TOTAL: {:>9}, ELAPSED: {} s, RATE: {}",
         format_bytes(total_bytes_read),
         elapsed.as_secs_f64(),
         transfer_rate,
